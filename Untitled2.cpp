78fbdb78388f955cc69b9a8d0ab7809fe64a43e3
tuananh26804dx <tuananh26804dx@gmail.com>
2025-11-06 09:53:38 +0700

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include<math.h>
+ int main(){
+ 	int arr[100];
+ 	int choose;
+ 	int n; arr[1];
+ 	int sum = 0;
+ 	int arr;
+ 	
+ 	while(choose){
+ 		printf("............Menu............");
+ 		printf("1 Nhap so luong phan tu va gia tri cho tung mang");
+ 		printf("2 In ra gia tri cho cac phan tu trong mang");
+ 		printf("3 In ra cac so lon hon gia tri trung binh cua cac phan tu trong mang");
+ 		printf("4 In ra gia tri nho nhat cua cac phan tu trong mang");
+ 		printf("5 Them moi mot phan tu vao dau mang");
+ 		printf("6 Xoa phan tu tai mot vi tri cu the");
+ 		printf("7 Sap xep mang theo thu tu tan dan");
+ 		printf("8 Kiem tra su ton tai cua phan tu trong mang");
+ 		prinft("9 In ra cac phan tu xuat hien dung 2 lan trong mang");
+ 		printf("10 In ra so xuat hien cua 1 phan tu");
+	 }
+    switch(choose){
+    	case 1;
+    	printf("Nhap so luong phan tu");
+    	scanf("%d", %n);
+    	if(n=0, n<100){
+    		printf("Khong hop le");
+		}
+		printf("Nhap gia tri tung phan tu");
+		scanf("%d", arr[1]);
+		if(n = 0 , n < 100){
+			printf("Khong hop le");
+			break;
+		}
+		case 2;
+		for(int i=0, i<100, i++){
+			printf("Cac gia tri trong mang:\n");
+			scanf("%d", %arr[1]);
+			break;
+		}
+		case 3;
+		if(n=0 ){
+		   printf("Tong cac phan tu le la:\n", n=0);
+		   
+		   else{
+		   	printf("Tong cac phan tu le la:\n" n=0);
+		   	break;
+		   }
+		case 4;
+		for(int i=0, i++){
+		   printf("Gia tri lon nhat:\n", n=0);
+		   break;
+		}
+		case 5;
+		for(int i=0, i<100, i++){
+		   for(int j=0, i<j, i++){
+		   }
+		}
+	    case 6;
+		case 7;
+		for(int i=0, i<100, i++){	
+		   arr=[1] = arr[j];
+		}	   
+    return 0;
+}
